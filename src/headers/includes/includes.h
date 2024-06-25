7ada8e8e407afdd18185d3c7075210808b0e96f7
xec <imraneqbit@proton.me>
2024-06-25 18:25:13 +0000

    fixed lib cjson including

--- src/headers/includes/includes.h
+++ src/headers/includes/includes.h
@@ -2,13 +2,14 @@
 #define INCLUDES_H
 
 
+#include <cjson/cJSON.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <bsd/string.h>
 #include <math.h>
 #include <ctype.h>
-#include "cJSON.h"
+// #include "cJSON.h"
 #include "eror_handling.h"
 #include "initialize.h"
 #include "defined_values.h"
